c8fbdb2afd8cd5c0a22d07a528657d61ee0bb8aa
hemavathim988 <hemavathi23cs045@drttit.edu.in>
2026-09-06 19:12:10 +0530

    Time: 1919 ms (5.05%) | Memory: 108.5 MB (5.35%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def numDistinct(self, s, t):
+        memo = {}
+
+        def solve(i, j):
+            if j == len(t):
+                return 1
+
+            if i == len(s):
+                return 0
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if s[i] == t[j]:
+                ans = solve(i + 1, j + 1) + solve(i + 1, j)
+            else:
+                ans = solve(i + 1, j)
+
+            memo[(i, j)] = ans
+            return ans
+
+        return solve(0, 0)
